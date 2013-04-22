7d61ea9e8891f90ed89c852d0886571c9edc8944
Раде Десподовски <rade.despodovski@gmail.com>
2013-04-22 16:38:22 +0200

    added tests for linkedin but still cannot perform the tests

--- test/index.js
+++ test/index.js
@@ -124,8 +124,55 @@ describe('oauth flow', function() {
             });
 
     });
+    it('should work for oauth 2.0 on linkedin', function(done) {
+        //return done();
+        var flow = oauthFlow({
+            provider: {
+                authorizationUrl: function (oauth) {
+                    return "https://www.linkedin.com/uas/oauth2/authorization?client_id=" + oauth.clientId +
+                        "&redirect_uri=" + oauth.oauth_callback +
+                        "&state=DCEEFFF45453sdffef424" +
+                        "&response_type=code";
+                },
+                accessTokenUrl: "https://www.linkedin.com/uas/oauth2/accessToken",
+                version: "2.0"
+            },
+            user: {
+                clientId: "b778bq6xnrzm",
+                clientSecret: "oG3pFhfMUfRLpbiX"
+            }
+        }, function (req, res) {
+            expect(req.query).to.have.property('test');
+            expect(req.query).to.have.property('other');
+            expect(req.oauth).to.have.property('oauth_access_token');
+            expect(req.oauth).to.have.property('oauth_refresh_token');
+            res.end('done');
+            done();
+        });
+
+        app.use('/auth/linkedin', flow);
+
+        var browser = new zombie({
+            userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31',
+            debug: true
+        });
+        browser.visit("http://localhost:3000/auth/linkedin?test=blah&other=test")
+            //browser.visit("http://localhost:3000/auth/box/start?test=blah&other=test")
+            .then(function () {
+                console.log("Redirected, filling up text");
+                // Fill email, password and submit form
+                return browser.
+                    fill("session_key",        "m8r-sphnmi@mailinator.com").
+                    fill("session_password",   "test123").
+                    pressButton("Allow access");
+            }).then(function() {
+                console.log("Done, redirecting back");
+            }, function(err) {
+                throw err;
+            });
+
+    });
     after(function(done) {
         server.close(done);
     });
-
 });
